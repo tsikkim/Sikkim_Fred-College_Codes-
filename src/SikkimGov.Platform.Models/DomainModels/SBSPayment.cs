c8372caee74e0d24a68c3aa607e3aebfdb3ac556
Chetan Ranpariya <ranpariyachetan.ad@gmail.com>
2020-05-25 14:51:55 +0800

    Added domain models for SBSPayment and SBS Receipt.

---
+++ src/SikkimGov.Platform.Models/DomainModels/SBSPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SikkimGov.Platform.Models.DomainModels
+{
+    public class SBSPayment
+    {
+        public long Id { get; set; }
+        public string BranchName { get; set; }
+        public string BranchCode { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime ChequeDate { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public long ChequeNumber { get; set; }
+        public decimal ChequeAmount { get; set; }
+        public string PlanType { get; set; }
+        public string MajorHead { get; set; }
+        public int PAOCode { get; set; }
+        public string IsWorks { get; set; }
+    }
+}
---
+++ src/SikkimGov.Platform.Models/DomainModels/SBSReceipt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SikkimGov.Platform.Models.DomainModels
+{
+    public class SBSReceipt
+    {
+        public long Id { get; set; }
+        public string BranchName { get; set; }
+        public string BranchCode { get; set; }
+        public string ChallanNumber { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string MajorHead { get; set; }
+        public string ReceiptPayment { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
